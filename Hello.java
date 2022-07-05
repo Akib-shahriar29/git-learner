85cd5753abac96edabe04e3954c1fa346495a3b6
Akib shahriar <akib.shahriar10@gmail.com>
2022-07-04 23:35:46 -0700

    added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(string []args) {
         system.out.println('Hello world!');
+        greeting("Akib shahriar")
     }
 }
+  public static void greeting(string name) {
+      
+      system.out.pritnln("welcome" + name);
+  }
